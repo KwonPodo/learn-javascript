785db345bce1c52a8c045f248af9aa777b37f1d5
Destktop <ludin9901@gmail.com>
2021-09-03 16:14:20 +0900

    modify async.js

--- async.js
+++ async.js
@@ -4,9 +4,9 @@
 // Execute the code in order after hoisting
 // hoisting: var, function declaration goes to the top in the JS Parser(Only Declarations)
 console.log('1');
-setTimeout(() => console.log('2'), 1000); //브라우저 내부에서 비동기적으로 처리 CallBack function
-console.log('3');
-
+setTimeout(() => console.log('2'), 0); //브라우저 내부에서 비동기적으로 처리 CallBack function
+console.log('3'); // setTimeout() 메소드를 위임받아 처리한 브라우저 프로그램은 
+                  // 비동기적 API를 제외한 모든 코드가 실행된 이후에 결과를 콘솔에 찍는다
 // Synchronous callback
 function printImmediately(print) {
     print();
@@ -45,6 +45,11 @@ class UserStorage {
         }, 1000);
     }
 }
+
+// 1. 사용자에게 id, password를 입력받는다.
+// 2. 받아온 id, password으로 login 한다
+// 3. 로그인이 성공적으로 된다면 사용자에게 id를 받아와서 roles를 요청한다
+// 4. roles가 성공적으로 받아와진다면 사용자의 object와 roles가 들어있는 것을 화면에 출력한다.
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
