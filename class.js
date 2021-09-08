e48df5553151d7c8af4f40a9a521c60f2c338b1a
Jgram15 <ludin9901@gmail.com>
2021-09-08 16:01:13 +0900

    modify clas.js

--- class.js
+++ class.js
@@ -126,3 +126,38 @@ console.log(triangle instanceof Rectangle); //false
 console.log(triangle instanceof Triangle); //true
 console.log(triangle instanceof Shape); //true
 console.log(triangle instanceof Object); //true
+
+
+
+
+// Class & Callback function
+// Class에 원하는 기능을 모두 정의하게 되면 사용자가 세부내용을 컨트롤할 수 없고 재사용성이 떨어진다.
+// 하지만 Callback 함수를 이용해서 Class를 만들게 되면, 사용자가 자신의 입맛에 맞게 재활용 가능해진다.
+// Class를 하나의 완전체로 만들기 보단, 원하는 기능을 끼워맞춰서 재조립이 가능하도록 설계하라.
+class Counter {
+    constructor(runEveryFiveTimes) {
+        this.counter = 0;
+        this.callback = runEveryFiveTimes;
+    }
+
+    increase() {
+        this.counter++;
+        console.log(this.counter);
+        if(this.counter % 5 === 0) {
+            this.callback && this.callback(this.counter);
+            // if(this.callback) {
+            //     this.callback(this.counter);
+            // }
+        }
+    }
+}
+
+function printSomething(num) {
+    console.log(`Wow! ${num}`);
+}
+function alertNum(num) {
+    alert(`alert! ${num}`);
+}
+
+const printCounter = new Counter(printSomething);
+const alertCounter = new Counter(alertNum);
