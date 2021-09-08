32ef1ef783f53bf37b1d6cae3b6c3a99b72ba3b2
Jgram15 <ludin9901@gmail.com>
2021-09-08 15:10:52 +0900

    modify function-basis.js

--- function-basic.js
+++ function-basic.js
@@ -1,6 +1,8 @@
 // Don't give up
 // 포기하지 마세요
 
+// 함수의 이유: 반복의 회피, 반복되는 작업의 재사용을 통한 효율적인 코드짜기.
+
 // 함수 선언
 // 함수 호출
 
@@ -27,6 +29,9 @@ function functionParam(add) {
 }
 
 // functionParam(add()) : 함수를 전달하는 것이 아닌, add함수를 functionParam보다 먼저 호출하는 것
+// 함수의 이름을 변수에 할당하거나, 또는 다른 함수의 인자로 전달한다는 것은
+// 그 함수의 이름이 가리키고 있는 함수의 reference를 전달하는 것과 같다.
+
 functionParam(add); // 함수를 전달할 때는 함수의 이름만을 명시하여 전달할 것.
 
 const addFun = add;
